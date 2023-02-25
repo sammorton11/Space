7d0d8f165f691fcc2302ae8147f2dc9a23a53418
sammorton11 <sammorton11@yahoo.com>
2023-02-25 10:00:37 -0600

    UI Changes

--- app/src/main/java/com/example/space/presentation/nasa_media_library/details_screen/DetailsScreenContent.kt
+++ app/src/main/java/com/example/space/presentation/nasa_media_library/details_screen/DetailsScreenContent.kt
@@ -1,11 +1,7 @@
 package com.example.space.presentation.nasa_media_library.details_screen
 
 import android.util.Log
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
@@ -14,13 +10,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import androidx.core.net.toUri
 import com.example.space.presentation.nasa_media_library.library_search_screen.components.cards.CardImage
 import com.example.space.presentation.nasa_media_library.library_search_screen.components.cards.CardVideo
 import com.example.space.presentation.nasa_media_library.library_search_screen.components.other.AudioPlayer
 import com.example.space.presentation.view_model.VideoDataViewModel
 import java.net.URLDecoder
-import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
 @Composable
@@ -47,8 +41,7 @@ fun DetailsScreenContent(
                     Text(text = decodedDescription)
                 }
                 "audio" -> {
-                    Log.d("Putting this url into Audio Player Comp", url)
-                    AudioPlayer(url, viewModel)
+                    AudioPlayer(viewModel)
                     Text(text = decodedDescription)
                 }
                 "image" -> {
